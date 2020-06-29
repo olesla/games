cd237773f31b6e9d37229911ac5308205386ebe5
Ole Christian Slaattene <slaattene@gmail.com>
2020-06-29 21:15:15 +0200

    init work

--- index.js
+++ index.js
@@ -1,88 +1,51 @@
-const container = document.querySelector('.container');
-const game      = container.querySelector('.game');
-const rows      = Array.from(container.querySelectorAll('.row'));
-const slots     = Array.from(container.querySelectorAll('.slot'));
-const winner    = container.querySelector('.winner');
-const currentP  = container.querySelector('.current-player');
-let player      = 1;
-let gameOver    = false;
+class Game {
+  /**
+   * Constructor
+   * @param {Array}  words 
+   * @param {Object} container
+   */
+  constructor(words, container) {
+    this.words         = words;
+    this.container     = container;
+    this.currentWord   = 0;
+    this.currentLetter = 0;
 
-container.addEventListener('click', event => {
-  if (event.target.classList.contains('slot'))
-    return playerMove(event.target);
+    this.elements = {
+      paragraph: document.createElement('p'),
+      canvas   : document.createElement('canvas'),
+    };
 
-  if (event.target.classList.contains('reset-button'))
-    return resetGame();
-});
+    for (const element of Object.values(this.elements))
+      this.container.appendChild(element);
 
-const playerMove = element => {
-  // Slot already taken
-  if (element.classList.contains('active') || gameOver)
-    return;
+    window.addEventListener('keydown', event => {
+      if (this.words[this.currentWord][this.currentLetter] === event.key)
+        console.log('correct');
+      else
+        console.log('wrong');
 
-  element.classList.add(`player-${player}`, 'active');
-  checkForWinner(player);
-  player = player === 1 ? 2 : 1;
-  currentP.innerText = `Player: ${player}`;
-};
+      this.currentLetter++;
 
-const checkVertical = () => {
-  let counter = 0;
-
-  for (const row of rows) {
-    for (const slot of row.children) {
-      if (slot.classList.contains(`player-${player}`)) {
-        counter++;
-        if (counter === 4)
-          return true;
-      } else {
-        counter = 0;
+      if (this.currentLetter === this.words[this.currentWord].length) {
+        this.currentWord++;
+        this.currentLetter = 0;
+        this.nextWord();
       }
+    });
+
+    this.nextWord();
+  }
+  
+  nextWord() {
+    if (this.currentWord >= this.words.length) {
+      this.elements.paragraph.innerText = 'You win!';
+      window.removeEventListener('keydown');
+    }
+    else {
+      this.elements.paragraph.innerText = this.words[this.currentWord];
     }
   }
+}
 
-  return false;
-};
-
-const checkHorizontal = () => {
-  let counter = 0;
-
-  for (let i = 0; i < 7; i++) {
-    for (const row of rows) {
-      if (row.children[i] && row.children[i].classList.contains(`player-${player}`)) {
-        counter++;
-        if (counter === 4)
-          return true;
-      } else {
-        counter = 0;
-      }
-    }
-  }
-
-  return false;
-};
-
-const checkForWinner = player => {
-  let result = 0;
-
-  const verticalResult   = checkVertical();
-  const horizontalResult = checkHorizontal();
-
-  if (verticalResult || horizontalResult) {
-    endGame(player);
-  }
-};
-
-const endGame = player => {
-  winner.innerText = `Player ${player} wins!`;
-  gameOver = true;
-};
-
-const resetGame = () => {
-  for (const slot of slots)
-    slot.classList.remove('active', 'player-1', 'player-2')
-
-  gameOver = false;
-  winner.innerText = '';
-  currentP.innerText = 'Player: 1';
-};
+const elem = document.querySelector('.container');
+const game = new Game(['ole', 'christian', 'slaattene'], elem);
